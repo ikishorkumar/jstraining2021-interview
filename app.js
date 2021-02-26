a5101d011622dbbf7c6cdc0e32234992d4afa268
ikishorkumarofficial <ikishorkumarofficial@gmail.com>
2021-02-26 23:38:57 +0500

    new changes

--- app.js
+++ app.js
@@ -61,13 +61,13 @@ document.getElementById("convertbtn").addEventListener("click", () => {
   if (valuetep != "" && inputtemp != "") {
     if (valuetep == "celsius") {
       let celtofah = Math.round((inputtemp * 9) / 5 + 32);
-      console.log(celtofah);
+      // console.log(celtofah);
       document.getElementById(
         "outputtemp"
       ).innerHTML = ` ${celtofah} fahrenheit `;
     } else {
       let fahtocel = Math.round(((inputtemp - 32) * 5) / 9);
-      console.log(fahtocel);
+      // console.log(fahtocel);
 
       document.getElementById("outputtemp").innerHTML = `${fahtocel}  celsius`;
     }
@@ -76,92 +76,140 @@ document.getElementById("convertbtn").addEventListener("click", () => {
   }
 });
 
+// Add Note Application
+
+
+// Saving To LocalStorage Function
+const saveLSD = () => {
+
+  const textnotes = document.querySelectorAll("textarea");
+  const allNotes = [];
+  // console.log(textnotes)
+  textnotes.forEach((onenote) => {
+    if (onenote.value !=="") {
+
+      return allNotes.push(onenote.value);
+    }
+  });
+
+
+  localStorage.setItem("allNotes", JSON.stringify(allNotes));
+};
+
 // Add Note
 
-// note list
-let notelist = document.getElementById("notelist");
+function addNoteitems(text = "") {
+  // note list
+  let notelist = document.getElementById("notelist");
 
+  // note card
+  let noteitem = document.createElement("div");
+  noteitem.setAttribute("id", "container");
+  noteitem.setAttribute("class", "tr");
+
+  //
+  let btnsave = document.createElement("button");
+  btnsave.innerText = "save";
+  btnsave.setAttribute("id", "save");
+  btnsave.setAttribute("class", "save");
+  btnsave.setAttribute("class", "hide");
+
+  //
+  let btndelete = document.createElement("button");
+  btndelete.setAttribute("id", "delete");
+  btndelete.setAttribute("class", "delete");
+  btndelete.innerText = "delete";
+
+  //
+  let btnedit = document.createElement("button");
+  btnedit.setAttribute("id", "edit");
+  btnedit.setAttribute("class", "edit");
+  btnedit.innerText = "edit";
+
+  // result Div for TeXt
+  let textdiv = document.createElement("div");
+  textdiv.setAttribute("id", "textdiv");
+  textdiv.setAttribute("class", "textdiv");
+  text.value ? textdiv.setAttribute("class", "hide") : "";
+
+  // text area
+  let txtarea = document.createElement("textarea");
+  txtarea.setAttribute("id", "note");
+  txtarea.setAttribute("class", "note");
+  txtarea.setAttribute("rows", "10");
+  txtarea.setAttribute("cols", "30");
+  text.value ? "" : txtarea.setAttribute("class", "hide");
+
+  // Appending elements to Item
+
+  noteitem.append(btnsave);
+  noteitem.append(btndelete);
+  noteitem.append(btnedit);
+  noteitem.append(textdiv);
+  noteitem.append(txtarea);
+
+  // Delete Node item
+  btndelete.addEventListener("click", function () {
+    notelist.removeChild(this.parentNode);
+    saveLSD();
+  });
+
+  if (text !='') {
+    txtarea.value = text;
+    textdiv.innerHTML = text;
+  }
+
+  // Edit Item text area value ;
+  btnedit.addEventListener("click", function () {
+    textdiv.setAttribute("class", "hide");
+    txtarea.removeAttribute("class", "hide");
+    btnsave.removeAttribute("class", "hide");
+    btnedit.setAttribute("class", "hide");
+  });
+
+  // gettting Values
+  let valueenterd;
+
+  txtarea.addEventListener("change", (event) => {
+    valueenterd = event.target.value;
+  });
+
+  // Save Item
+  btnsave.addEventListener("click", function () {
+    btnedit.removeAttribute("class", "hide");
+    textdiv.removeAttribute("class", "hide");
+
+    if(txtarea.value !=''){
+      textdiv.innerHTML = valueenterd;
+    }
+    else{
+      notelist.removeChild(this.parentNode);
+
+    }
+    
+    saveLSD();
+
+    btnsave.setAttribute("class", "hide");
+    txtarea.setAttribute("class", "hide");
+  });
+  notelist.appendChild(noteitem);
+}
+
+
+// Getting Local Storage Data
+const recivednotes = JSON.parse(localStorage.getItem("allNotes"));
+if (recivednotes) {
+  recivednotes.forEach((recivednote) => {
+    if(recivednote!=''){
+      addNoteitems(recivednote);
+
+    }
+
+  });
+}
+
+
+// Adding On Clock on AddNote BTN
 document
   .getElementById("addnote")
-  .addEventListener("click", (text = "kishor") => {
-    // note card
-    let noteitem = document.createElement("div");
-    noteitem.setAttribute("id", "container");
-    noteitem.setAttribute("class", "tr");
-
-    //
-    let btnsave = document.createElement("button");
-    btnsave.innerText = "save";
-    btnsave.setAttribute("id", "save");
-    btnsave.setAttribute("class", "save");
-    btnsave.setAttribute("class", "hide");
-
-    //
-    let btndelete = document.createElement("button");
-    btndelete.setAttribute("id", "delete");
-    btndelete.setAttribute("class", "delete");
-    btndelete.innerText = "delete";
-
-    //
-    let btnedit = document.createElement("button");
-    btnedit.setAttribute("id", "edit");
-    btnedit.setAttribute("class", "edit");
-    btnedit.innerText = "edit";
-
-    // text area
-    let textdiv = document.createElement("div");
-    textdiv.setAttribute("id", "textdiv");
-    textdiv.setAttribute("class", "textdiv");
-    text.value ? textdiv.setAttribute("class", "hide") : "";
-    console.group(text);
-    // text area
-    let txtarea = document.createElement("textarea");
-    txtarea.setAttribute("id", "note");
-    txtarea.setAttribute("class", "note");
-    txtarea.setAttribute("rows", "10");
-    txtarea.setAttribute("cols", "30");
-    text.value ? "" : txtarea.setAttribute("class", "hide");
-
-    //
-
-    noteitem.append(btnsave);
-    noteitem.append(btndelete);
-    noteitem.append(btnedit);
-    noteitem.append(textdiv);
-    noteitem.append(txtarea);
-
-    // Delete Node item
-    btndelete.addEventListener("click", function () {
-      notelist.removeChild(this.parentNode);
-    });
-
-  
-    // Edit Item text area value ;
-    btnedit.addEventListener("click", function () {        
-      textdiv.setAttribute("class","hide");
-      txtarea.removeAttribute("class","hide");
-      btnsave.removeAttribute("class","hide");
-      btnedit.setAttribute("class","hide");
-    });
-
-
-    // Save Item
-    btnsave.addEventListener("click", function () {
-      
-        btnedit.removeAttribute("class","hide");
-        textdiv.removeAttribute("class","hide");
-
-
-
-
-        textdiv.innerHTML = txtarea.value; 
-
-
-        btnsave.setAttribute("class","hide");
-        txtarea.setAttribute("class","hide");
-
-     
-
-    });
-    notelist.appendChild(noteitem);
-  });
+  .addEventListener("click", () => addNoteitems());
